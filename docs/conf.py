0beeecacc0a934e80d052522ac3309884324378f
Alexandre Gerona <alecgerona@gmail.com>
2020-06-13 20:53:48 +0800

    Get rid of recommonmark

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Alexandre Gerona & Jasper Sibayan"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
